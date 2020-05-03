62a5b6af0241efbc8b389876d5e0f6cede729150
Tim Hobson <claehobson@gmail.com>
2020-05-03 23:42:43 +1000

    added bricks

--- main.js
+++ main.js
@@ -5,11 +5,44 @@ let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
 let ballRadius = 10;
+
 let paddleHeight = 10;
 let paddleWidth = 75;
 let paddleX = (canvas.width-paddleWidth) / 2;
 let rightPressed = false;
 let leftPressed = false;
+
+let brickRowCount = 3;
+let brickColumnCount = 5;
+let brickWidth = 75;
+let brickHeight = 20;
+let brickPadding = 10;
+let brickOffsetTop = 30;
+let brickOffsetLeft = 30;
+
+let bricks = [];
+for (let c = 0; c < brickColumnCount; c++) {
+  bricks[c] = [];
+  for (let r = 0; r < brickRowCount; r++) {
+    bricks[c][r] = { x:0, y:0 };
+  }
+}
+
+function drawBricks() {
+  for (let c = 0; c < brickColumnCount; c++) {
+    for (let r = 0; r < brickRowCount ; r++) {
+      let brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+      let brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+      bricks[c][r].x = brickX;
+      bricks[c][r].y = brickY;
+      ctx.beginPath();
+      ctx.rect(brickX, brickY, brickWidth, brickHeight);
+      ctx.fillStyle = 'cyan';
+      ctx.fill();
+      ctx.closePath;
+    }
+  }
+}
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
@@ -51,8 +84,7 @@ function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
   drawBall();
   drawPaddle();
-  x += dx;
-  y += dy;
+  drawBricks();
   if (rightPressed) {
     paddleX += 7;
     if (paddleX + paddleWidth > canvas.width) {
@@ -67,11 +99,21 @@ function draw() {
   if (x + dx < ballRadius || x + dx > canvas.width - ballRadius) {
     dx = -dx;
   }
-  if (y + dy < ballRadius || y + dy > canvas.height - ballRadius) {
+  if (y + dy < ballRadius) {
     dy = -dy;
+  } else if (y + dy > canvas.height - ballRadius) {
+    if (x > paddleX && x < paddleX + paddleWidth) {
+      dy = -dy;
+    } else {
+      // alert("GAME OVER");
+      // document.location.reload();
+      // clearInterval(interval);
+    }
   }
+  x += dx;
+  y += dy;
 };
 
 
 
-setInterval(draw, 10);
+var interval = setInterval(draw, 10);
