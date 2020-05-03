5d054ccca0ec434844c83e534549535218bb3f5a
Tim Hobson <claehobson@gmail.com>
2020-05-03 23:09:23 +1000

    ball moving and colliding with edges

--- main.js
+++ main.js
@@ -1,22 +1,32 @@
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
 let x = canvas.width / 2;
 let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
+const ballRadius = 10;
 
 function drawBall() {
   ctx.beginPath();
-  ctx.arc(x, y, 10, 0, Math.PI*2);
+  ctx.arc(x, y, ballRadius, 0, Math.PI*2);
   ctx.fillStyle  = "fuchsia";
   ctx.fill();
   ctx.closePath();
-}
+};
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
   drawBall();
   x += dx;
   y += dy;
-}
+  if (x + dx < ballRadius || x + dx > canvas.width - ballRadius) {
+    dx = -dx;
+  }
+  if (y + dy < ballRadius || y + dy > canvas.height - ballRadius) {
+    dy = -dy;
+  }
+};
+
+
+
 setInterval(draw, 10);
